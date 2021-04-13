de19fb29eb984d0b461d6dd47da4a0fd06eb98b2
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-13 00:24:47 +0000

    Fixed .to_s causing grade to fail

--- loops_letter_count.rb
+++ loops_letter_count.rb
@@ -15,10 +15,9 @@
 
 p "Enter a word:"
 word = gets.chomp
-wordarray = word.split("")
-counter = 1
-while counter <= wordarray.count
-  p counter.to_s
-  counter = counter + 1
+word_length_counter = 1
+while word_length_counter <= word.length
+  p word_length_counter
+  word_length_counter = word_length_counter + 1
 end
-p "#{word} is #{wordarray.count} letters long!"
+p "#{word} is #{word.length} letters long!"
